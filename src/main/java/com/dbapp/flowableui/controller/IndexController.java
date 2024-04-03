abf27d0522296c68e841b3f692bfa23a3af61522
jinxin <jinxin70@gmail.com>
2024-04-03 10:56:42 +0800

    [add] 引入knife4j管理api和页面调试

---
+++ src/main/java/com/dbapp/flowableui/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.dbapp.flowableui.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author JinXin
+ * @date 2024/4/3 10:32
+ * @description
+ */
+@Api(tags = "首页模块")
+@RestController
+public class IndexController {
+
+    @ApiImplicitParam(name = "name",value = "姓名",required = true)
+    @ApiOperation(value = "问好")
+    @GetMapping("/sayHi")
+    public ResponseEntity<String> sayHi(@RequestParam(value = "name")String name){
+        return ResponseEntity.ok("Hi:"+name);
+    }
+}
