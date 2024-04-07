4f47367845f81de82fd9eb7d28ce54ae3828d3d1
jinxin <jinxin70@gmail.com>
2024-04-07 21:53:08 +0800

    [add] TaskDto抽出来

--- src/main/java/com/dbapp/flowableui/controller/MyRestController.java
+++ src/main/java/com/dbapp/flowableui/controller/MyRestController.java
@@ -1,5 +1,6 @@
 package com.dbapp.flowableui.controller;
 
+import com.dbapp.flowableui.modle.dto.TaskDto;
 import com.dbapp.flowableui.service.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -36,51 +37,4 @@ public class MyRestController {
         }).collect(Collectors.toList());
     }
 
-    class TaskDto {
-        private String id;
-        private String name;
-        private String assignee;
-        private String createTime;
-        private String processInstanceId;
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getAssignee() {
-            return assignee;
-        }
-
-        public void setAssignee(String assignee) {
-            this.assignee = assignee;
-        }
-
-        public String getCreateTime() {
-            return createTime;
-        }
-
-        public void setCreateTime(String createTime) {
-            this.createTime = createTime;
-        }
-
-        public String getProcessInstanceId() {
-            return processInstanceId;
-        }
-
-        public void setProcessInstanceId(String processInstanceId) {
-            this.processInstanceId = processInstanceId;
-        }
-    }
 }
---
+++ src/main/java/com/dbapp/flowableui/modle/dto/TaskDto.java
@@ -0,0 +1,53 @@
+package com.dbapp.flowableui.modle.dto;
+
+/**
+ * @author JinXin
+ * @date 2024/4/7 21:19
+ * @description
+ */
+public class TaskDto {private String id;
+    private String name;
+    private String assignee;
+    private String createTime;
+    private String processInstanceId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getProcessInstanceId() {
+        return processInstanceId;
+    }
+
+    public void setProcessInstanceId(String processInstanceId) {
+        this.processInstanceId = processInstanceId;
+    }
+}
