88b60173391ada98b01930e0169b5918df1a1c8c
jinxin <jinxin70@gmail.com>
2024-04-07 21:11:29 +0800

    [add] 集成mybatis-plus

---
+++ src/main/java/com/dbapp/flowableui/config/MybatisPlusConfig.java
@@ -0,0 +1,37 @@
+package com.dbapp.flowableui.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author JinXin
+ * @date 2024/4/7 17:35
+ * @description
+ */
+@Configuration
+@MapperScan(basePackages = "com.dbapp.flowableui.mapper",
+        sqlSessionFactoryRef = "sqlSessionFactory",
+        sqlSessionTemplateRef = "sqlSessionTemplate")
+public class MybatisPlusConfig {
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+//    @Bean(destroyMethod = "clearCache")
+//    @Qualifier("flowableModeler")
+//    public SqlSessionTemplate modelerSqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+//        return new SqlSessionTemplate(sqlSessionFactory);
+//    }
+
+}
