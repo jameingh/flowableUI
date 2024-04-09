8cd5741cb9cb079afddb0c68a4377f3e7ec9c2f3
jinxin <jinxin70@gmail.com>
2024-04-09 08:46:05 +0800

    [flowable] 设置监听用户任务start事件的监听器，在监听器里动态分配用户和组，查询用户的参与人

--- src/test/java/com/dbapp/flowableui/FlowableUiApplicationTests.java
+++ src/test/java/com/dbapp/flowableui/FlowableUiApplicationTests.java
@@ -1,21 +1,24 @@
 package com.dbapp.flowableui;
 
+import lombok.extern.slf4j.Slf4j;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.test.Deployment;
+import org.flowable.identitylink.api.IdentityLink;
+import org.flowable.task.api.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Slf4j
 @SpringBootTest
-@ContextConfiguration(classes = FlowableUiApplication.class)
 class FlowableUiApplicationTests {
 
     @Test
@@ -28,21 +31,51 @@ class FlowableUiApplicationTests {
     @Autowired
     private TaskService taskService;
 
+    // 在processes目录下的流程定义文件会自动部署，不需要特别声明
+//    @Deployment(resources = { "processes/one-task-process.bpmn20.xml" })
+    @Deployment
     @Test
-    @Deployment(resources = { "processes/one-task-process.bpmn20.xml" })
     @Transactional
     public void simpleProcessTest() {
         final HashMap<String, Object> formProperties = new HashMap<>();
         formProperties.put("dateVariable", "2024-04-03");
         final ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("oneTaskProcess", formProperties);
+        log.info("process instance id: " + processInstance.getId());
         assertEquals(1, runtimeService.createProcessInstanceQuery().count());
-//        Task task = taskService.createTaskQuery().singleResult();
-//        assertEquals("my task", task.getName());
+        Task task = taskService.createTaskQuery().singleResult();
+        assertEquals("my task", task.getName());
 
-//        taskService.complete(task.getId());
-//        assertEquals(0, runtimeService.createProcessInstanceQuery().count());
-        long count = taskService.createTaskQuery().count();
-        System.out.println(count);
+        taskService.complete(task.getId());
+        assertEquals(0, runtimeService.createProcessInstanceQuery().count());
+    }
+
+    @Test
+    @Deployment
+    @Transactional
+    public void taskListenerProcessTest() {
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("taskListenerProcess");
+        log.info("process instance id: " + processInstance.getId());
+        assertEquals(1, runtimeService.createProcessInstanceQuery().count());
+        Task task = taskService.createTaskQuery().singleResult();
+        assertEquals("my task", task.getName());
+
+        // 流程实例的任务数
+        final long count = taskService.createTaskQuery().processInstanceId(processInstance.getProcessInstanceId()).count();
+        assertEquals(1, count);
+
+        // 查询潜在用户和用户组(候选人和候选组)，参与人（即AssignmentListener中设置的所有人）
+        final List<IdentityLink> identityLinksForTask = taskService.getIdentityLinksForTask(task.getId());
+        for (IdentityLink identityLink : identityLinksForTask) {
+            log.info("identity link user: {}" ,identityLink.getUserId());
+        }
+//        taskService.claim(task.getId(), "kim");
+// 只能对unclaim的任务进行claim。否则报错already claimed by someone else
+        taskService.unclaim(task.getId());
+        taskService.claim(task.getId(), "tom");
+        task = taskService.createTaskQuery().singleResult();
+        log.info("task assignee: " + task.getAssignee());
+        taskService.complete(task.getId());
+        assertEquals(0, taskService.createTaskQuery().count());
     }
 
 }
