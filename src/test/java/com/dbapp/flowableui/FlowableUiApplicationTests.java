e51233064d3bea7d575a21726a709bbde54f392a
jinxin <jinxin70@gmail.com>
2024-04-10 09:07:08 +0800

    [flowable] 流程定义文件添加图形元素，输出完成的流程，高亮任务和线

--- src/test/java/com/dbapp/flowableui/FlowableUiApplicationTests.java
+++ src/test/java/com/dbapp/flowableui/FlowableUiApplicationTests.java
@@ -1,17 +1,29 @@
 package com.dbapp.flowableui;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.flowable.bpmn.model.BpmnModel;
+import org.flowable.engine.HistoryService;
+import org.flowable.engine.RepositoryService;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
+import org.flowable.engine.history.HistoricActivityInstance;
+import org.flowable.engine.history.HistoricProcessInstance;
+import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.test.Deployment;
 import org.flowable.identitylink.api.IdentityLink;
+import org.flowable.image.impl.DefaultProcessDiagramGenerator;
 import org.flowable.task.api.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -54,7 +66,8 @@ class FlowableUiApplicationTests {
     @Deployment
     @Transactional
     public void taskListenerProcessTest() {
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("taskListenerProcess");
+        String processDefinitionKey = "taskListenerProcess";
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processDefinitionKey);
         log.info("process instance id: " + processInstance.getId());
         assertEquals(1, runtimeService.createProcessInstanceQuery().count());
         Task task = taskService.createTaskQuery().singleResult();
@@ -84,6 +97,7 @@ class FlowableUiApplicationTests {
         log.info("task assignee: " + task.getAssignee());
         taskService.complete(task.getId());
         assertEquals(0, taskService.createTaskQuery().count());
+        generateProcessDiagram(processDefinitionKey, "taskListenerProcess.png");
     }
 
     /**
@@ -96,6 +110,41 @@ class FlowableUiApplicationTests {
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("serviceTaskProcess");
         log.info("process instance id: " + processInstance.getId());
         assertEquals(0, runtimeService.createProcessInstanceQuery().count());
+    }
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Autowired
+    private HistoryService historyService;
+
+    public void generateProcessDiagram(String processDefinitionKey, String filePath) {
+        ProcessDefinition pd = repositoryService.createProcessDefinitionQuery()
+                .processDefinitionKey(processDefinitionKey).latestVersion().singleResult();
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(pd.getId());
+        HistoricProcessInstance hpi = historyService.createHistoricProcessInstanceQuery().singleResult();
+        if (hpi == null) {
+            return;
+        }
+        List<String> highLightedActivities = new ArrayList<>();
+        List<String> hightLightedFlows = new ArrayList<>();
+        double scaleFactor = 1.0;
+        boolean drawSqquenceFlowNameWithNoLabelDI = true;
+        List<HistoricActivityInstance> list = historyService.createHistoricActivityInstanceQuery().processInstanceId(hpi.getId()).list();
+        for (HistoricActivityInstance hai : list) {
+            if (hai.getActivityType().equals("sequenceFlow")) {
+                hightLightedFlows.add(hai.getActivityId());
+            } else {
+                highLightedActivities.add(hai.getActivityId());
+            }
+        }
+        DefaultProcessDiagramGenerator generator = new DefaultProcessDiagramGenerator();
+        InputStream inputStream = generator.generateDiagram(bpmnModel, "PNG", highLightedActivities, hightLightedFlows, scaleFactor, drawSqquenceFlowNameWithNoLabelDI);
+        try {
+            FileUtils.copyInputStreamToFile(inputStream, new File(filePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
