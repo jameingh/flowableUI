d66a0fe87a8fcbd2c9e91307d805b76ae7791dc9
jinxin <jinxin70@gmail.com>
2024-04-24 16:04:26 +0800

    [flowable] flowable升级到 7.0.1，springdoc替换swagger，springboot从2.3.7升级到 3.1.6，jdk升级到 17，去掉UI依赖，升级knife4j到openapi3，升级mysql驱动为mysql-connector-j:8.3.0

--- src/main/java/com/kim/flowableui/controller/IndexController.java
+++ src/main/java/com/kim/flowableui/controller/IndexController.java
@@ -1,8 +1,9 @@
 package com.kim.flowableui.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2024/4/3 10:32
  * @description
  */
-@Api(tags = "首页模块")
+@Tag(name = "首页模块")
 @RestController
 public class IndexController {
 
-    @ApiImplicitParam(name = "name",value = "姓名",required = true)
-    @ApiOperation(value = "问好")
+    @Operation(summary = "问好")
+    @Parameters(@Parameter(name = "name",description = "姓名", required = true))
     @GetMapping("/sayHi")
     public ResponseEntity<String> sayHi(@RequestParam(value = "name")String name){
         return ResponseEntity.ok("Hi:"+name);
